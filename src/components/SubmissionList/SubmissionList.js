51d2dbf2d2a7357e92c5083064138e11f7fcc5d4
Rindy Nunes <RNUNES1210@GMAIL.COM>
2018-01-31 10:19:45 -0500

    handle api

--- src/components/SubmissionList/SubmissionList.js
+++ src/components/SubmissionList/SubmissionList.js
@@ -13,63 +13,29 @@ const oddStyle = {
   background: 'grey'
 }
 
-class SubmissionList extends Component {
 
-  constructor(props){
-      super(props)
-    }
-
-
-
-  onComponentDidMount(){
-    console.log('mounting SubmissionList')
-  }
-
-  let inputs = this.props.inputs.map( (input, index)=>
-        {
-          let style = {}
-          if(index%2) {style=evenStyle}
-          else {style=oddStyle}
-          return ( <SubmissionItem key={index}
-                                   style={style}
-                                   input={input}
-                                   handleUpdate={props.handleUpdate}/>)
-        })
+const SubmissionList = props => {
+  let inputs = props.inputs.map( (input, index)=>
+      {
+        let style = {}
+        if(index%2) {style=evenStyle}
+        else {style=oddStyle}
+        return ( <SubmissionItem key={index}
+                                 style={style}
+                                 input={input}
+                                 handleUpdate={props.handleUpdate}/>)
+      })
 
 
-render(){
-    return (
-      <div>
-       <h1>What's Your Weather Ware?</h1>
-       <button onClick={props.toggleForm}>Add Your Own Idea!</button>
-        {inputs}
-      </div>
-    )
+
+  return (
+    <div>
+     <h1>What's Your Weather Ware?</h1>
+     <button onClick={props.toggleForm}>Add Your Own Idea!</button>
+      {inputs}
+    </div>
+  )
+
 }
-}
-
-// const SubmissionList = props => {
-//   let inputs = props.inputs.map( (input, index)=>
-//       {
-//         let style = {}
-//         if(index%2) {style=evenStyle}
-//         else {style=oddStyle}
-//         return ( <SubmissionItem key={index}
-//                                  style={style}
-//                                  input={input}
-//                                  handleUpdate={props.handleUpdate}/>)
-//       })
-//
-//
-//
-//   return (
-//     <div>
-//      <h1>What's Your Weather Ware?</h1>
-//      <button onClick={props.toggleForm}>Add Your Own Idea!</button>
-//       {inputs}
-//     </div>
-//   )
-//
-// }
 
 export default SubmissionList
